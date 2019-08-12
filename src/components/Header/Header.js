67d0ef1cc34636ef8cb29a768039ca9605845238
aidanatee <aidana.bakhtiyer@gmail.com>
2019-08-12 12:53:06 -0400

    layout loads full page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,6 +20,7 @@ class Header extends PureComponent {
           )
           }
     </AuthConsumer>
+
     );
   }
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,8 +3,29 @@ import './Layout.css';
 
 class Layout extends PureComponent {
   render() {
-    return 'empty';
+   const { header: Header, footer: Footer, children } = this.props;
+
+   return (
+     <div>
+     <header class="header">
+     <p class="header__title section-title">Header</p>
+     <Header />
+     </header>
+     <main class="main main--with-header main--with-footer">
+     {children}</main>
+     <footer class="footer"><p class="header__title section-title">Footer</p>
+     <Footer/>
+     </footer>
+     
+     </div>
+
+
+   );
   }
 }
 
 export default Layout;
+
+
+
+
